9920c47711cd48ddedc71cb650f6f9ef43a4517a
SDETMatomeMasiye <masiyeholdings@gmail.com>
2025-06-10 21:08:31 +0200

    Adding support for mobile driver.

--- src/test/java/testauto/com/ui/utils/DriverUtil.java
+++ src/test/java/testauto/com/ui/utils/DriverUtil.java
@@ -1,5 +1,8 @@
 package testauto.com.ui.utils;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
@@ -9,15 +12,25 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import testauto.com.common.FilesUtil;
 import testauto.com.common.LogUtil;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Objects;
 
 import static testauto.com.common.FilesUtil.getPropertyValue;
 
 public class DriverUtil {
 
-    private static WebDriver driver;
+    private static WebDriver webDriver;
+    private static AppiumDriver mobileDriver;
+
     private static final String ARG_DELIMITER = "\\|";
 
     private static MutableCapabilities loadWebOptions(String propertiesPath) throws Exception {
@@ -54,30 +67,79 @@ public class DriverUtil {
         if(driverOptions instanceof ChromeOptions){
             ((ChromeOptions) driverOptions).setExperimentalOption("excludeSwitches", Arrays.asList(switches));
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver((ChromeOptions) driverOptions);
+            webDriver = new ChromeDriver((ChromeOptions) driverOptions);
         }else if(driverOptions instanceof EdgeOptions){
             ((EdgeOptions) driverOptions).setExperimentalOption("excludeSwitches", Arrays.asList(switches));
             WebDriverManager.edgedriver().setup();
-            driver = new EdgeDriver((EdgeOptions) driverOptions);
+            webDriver = new EdgeDriver((EdgeOptions) driverOptions);
         }else if(driverOptions instanceof FirefoxOptions){
             WebDriverManager.firefoxdriver().setup();
-            driver = new FirefoxDriver((FirefoxOptions) driverOptions);
+            webDriver = new FirefoxDriver((FirefoxOptions) driverOptions);
         }
-        Objects.requireNonNull(driver, "driver cannot be null.");
+        Objects.requireNonNull(webDriver, "webDriver cannot be null.");
         LogUtil.info("Driver set to '" + driverOptions.getBrowserName() + "'.", DriverUtil.class);
-        driver.manage().window().maximize();
+        webDriver.manage().window().maximize();
     }
 
-    public static WebDriver getWebDriver(){
-        if(driver == null){
-            throw new IllegalStateException("Cannot get driver because driver == null. Call initializeWebDriver(String) first.");
+    private static DesiredCapabilities getMobileCapabilities(String propertiesPath,String platformName, boolean isBrowser) throws IOException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String prefix = platformName + ".";
+        capabilities.setCapability("platformName", getPropertyValue(propertiesPath,prefix + "platformName"));
+        capabilities.setCapability("appium:deviceName", getPropertyValue(propertiesPath,prefix + "deviceName"));
+        capabilities.setCapability("appium:automationName", getPropertyValue(propertiesPath,prefix + "automationName"));
+        if(isBrowser){
+            capabilities.setCapability("browserName", getPropertyValue(propertiesPath,prefix + "browserName"));
+        }else{
+            capabilities.setCapability("app", getPropertyValue(propertiesPath,prefix + "app"));
         }
-        return driver;
+        capabilities.setCapability("appium:platformVersion", getPropertyValue(propertiesPath, prefix + "platformVersion"));
+        capabilities.setCapability("appium:noReset", getPropertyValue(propertiesPath, prefix + "noReset"));
+        return capabilities;
+    }
+
+
+    public static void initializeMobileDriver(String propertiesPath, boolean isBrowser) throws IOException {
+        String platformName = getPropertyValue(propertiesPath, "platform");
+        if(platformName == null || platformName.isBlank()) throw new RuntimeException("Platform cannot be null or empty.");
+        URL url = new URL(Objects.requireNonNull(getPropertyValue(propertiesPath, "appiumServer")));
+
+        switch (platformName.toLowerCase()){
+            case "android":
+                mobileDriver = new AndroidDriver(url, getMobileCapabilities(propertiesPath, platformName, isBrowser));
+                break;
+            case "ios":
+                mobileDriver = new IOSDriver(url, getMobileCapabilities(propertiesPath, platformName, isBrowser));
+                break;
+            default:
+                throw new UnsupportedOperationException("'" + platformName + "' is not a supported platform. Supported platforms include: ios and android.");
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends WebDriver> T getDriver(Class<T> targetDriver){
+        String errorMessage = "Couldn't get '" + targetDriver + "' webDriver because %s == null. Call %s(String propertiesPath) first.";
+        if(IOSDriver.class.isAssignableFrom(targetDriver) || AndroidDriver.class.isAssignableFrom(targetDriver)){
+            Objects.requireNonNull(mobileDriver, String.format(errorMessage, "mobileDriver", "initializeMobileDriver"));
+            return (T) mobileDriver;
+        }else if(WebDriver.class == targetDriver){
+            Objects.requireNonNull(webDriver, String.format(errorMessage, "webDriver", "initializeWebDriver"));
+            return (T) webDriver;
+        }else{
+            throw new IllegalArgumentException(targetDriver + " is not a supported option. Supported options include: ios, android and web.");
+        }
     }
 
     public static void quitDriver(){
-        if(driver != null){
-            driver.quit();
+        if(webDriver != null) {
+            webDriver.quit();
+            webDriver = null;
+            LogUtil.info("Web Driver is quit and set to null.", DriverUtil.class);
+        }
+
+        if(mobileDriver != null){
+            mobileDriver.quit();
+            mobileDriver = null;
+            LogUtil.info("Appium Driver is quit and set to null.", DriverUtil.class);
         }
     }
 
